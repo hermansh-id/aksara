26ab579dc86a21ff8ec4952a41ec73c28fdbd949
hermansh-id <hermansh.id@gmail.com>
2023-02-05 00:45:10 +0700

    initial commit

---
+++ src/main.py
@@ -0,0 +1,30 @@
+from yolov5.detect_class import predict
+import streamlit as st
+import os
+from PIL import Image
+import shutil
+
+st.title("Object Detection for Aksara")
+
+mode = st.selectbox("Pilih aksara", ["jawa"])
+upload_file = st.file_uploader("Pilih gambar", type=["png", "jpg"])
+mulai = st.button("Prediksi")
+
+if mulai and upload_file:
+    
+    path_this = os.path.realpath(os.path.dirname(__file__))
+    dir_img = os.path.join(path_this, "uploads")
+    
+    if(os.path.exists(dir_img)):
+        shutil.rmtree(dir_img)
+        os.mkdir(dir_img)
+    
+    
+    with open(os.path.join(path_this, "uploads", upload_file.name),"wb") as f:
+        f.write(upload_file.read())
+    st.header("Gambar awal")
+    st.image(upload_file)
+    predict("uploads/"+upload_file.name, "jawa")
+    st.header("Gambar di prediksi")
+    image = Image.open(os.path.join(path_this, 'prediction', upload_file.name))
+    st.image(image)
